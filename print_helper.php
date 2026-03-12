19a0e1b60ead4b1797104b2509f6eafd8565d32c
Kuntur Studio <kunturstudio@gmail.com>
2026-03-12 14:01:26 -0300

    ajustes de impresión

--- print_helper.php
+++ print_helper.php
@@ -15,17 +15,17 @@ $ref_text = clean($p['reference']);
 $name = strtoupper(clean($p['name']));
 $barcode = clean($p['barcode']);
 
-$width = "50mm"; 
+$width = "52mm"; 
 
 // 1. CABECERA: Padding lateral cero
-$html_header = "<div style='width:$width; background-color:#000; color:#fff; padding:1.5mm 0; font-size:3.5mm; font-weight:bold; text-align:center; box-sizing:border-box;'>" . 
+$html_header = "<div style='width:$width; background-color:#000; color:#fff; padding:1.5mm; font-size:3.5mm; font-weight:bold; text-align:center; box-sizing:border-box;'>" . 
                htmlspecialchars($name) . 
                "</div>";
 
 // 2. PRECIO: Dinámico en mm
 $price_len = strlen($price);
-// Si es muy largo (>7 caracteres), reducimos el tamaño a 8mm, sino 10mm
-$f_size = ($price_len > 7) ? "8mm" : "10mm";
+// Si es muy largo (>8 caracteres), reducimos el tamaño a 8mm, sino 10mm
+$f_size = ($price_len > 8) ? "8mm" : "10mm";
 
 $html_price = "<div style='width:$width; text-align:center; padding:2mm 0; font-size:$f_size; font-weight:900; line-height:1; box-sizing:border-box;'>" . 
               "$" . $price . 
